be088b95bd807c972412e344982ad905fc390fdc
dr0n <kornienko.andriy@gmail.com>
2020-10-03 19:36:05 +0300

    add range-slider

--- app/js/main.js
+++ app/js/main.js
@@ -100,3 +100,68 @@
         $(this).next('.tooltip').remove();
 
     });
+
+
+    //custom range slider
+
+    $.fn.WBslider = function() {
+        return this.each(function() {
+            var $_this = $(this),
+                $_input = $('input', $_this),
+                $_current_value = $('.current-value', $_this),
+                $_min_value = $_input.attr('min'),
+                $_max_value = $_input.attr('max'),
+                thumbwidth = 30; // set this to the pixel width of the thumb
+
+            // set range max to current year
+            $_input.attr('max', $_max_value);
+            $('.max-value span', $_this).text($_max_value);
+            //$_input.val($_max_value - 10);
+
+            $_input.on('input change keyup', function() {
+                var $_this = $(this),
+                    val = parseInt($_input.val(), 10);
+
+                if (val < 30) {
+                    val = '< 31';
+                }
+                if (val === '') { // Stop IE8 displaying NaN
+                    val = 0;
+                }
+
+                $_current_value.text( val );
+
+                var pos = (val - $_input.attr('min'))/($_input.attr('max') - $_input.attr('min'));
+
+                console.log('pos=' +pos);
+
+                // position the title with the thumb
+                var thumbCorrect = thumbwidth * (pos - 0.5) * -1,
+                    titlepos = Math.round( ( pos * $_input.width() ) - thumbwidth/4 + thumbCorrect );
+
+                console.log(titlepos);
+
+                $_current_value.css({'left': titlepos});
+
+                // show "progress" on the track
+                pos = Math.round( pos * 99 ); // to hide stuff behide the thumb
+                var grad = 'linear-gradient(90deg, #1f5e9e ' + pos + '%,#dbdcde ' + (pos+1) + '%)';
+                $_input.css({'background': grad});
+
+            }).on('focus', function() {
+                if ( isNaN( $(this).val() ) ) {
+                    $(this).val(0);
+                }
+            }).trigger('change');
+
+            $(window).on('resize', function() {
+                $_input.trigger('change');
+            });
+        });
+    };
+
+    $(function() {
+
+        $('.slider').WBslider();
+
+    });
